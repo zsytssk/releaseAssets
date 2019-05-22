0ba13b6c41b2cdb9411f6cb5e68a132b917ce391
张世阳 <ZHANGSHIYANG162@pingan.com.cn>
2019-05-22 14:57:51 +0800

    master:[update]

--- script/const.ts
+++ script/const.ts
@@ -1,6 +1,6 @@
 import * as config from './config.json';
 
-export const PROJECT_FOLDER = config.project_folder;
+export const project_folder = config.project_folder;
 export const bin = config.bin;
 export const layaAssets = config.layaAssets;
 export const binJs = config.binJs;
--- script/releaseAssets.ts
+++ script/releaseAssets.ts
@@ -1,14 +1,19 @@
 import * as path from 'path';
-import { bin, binJs, layaAssets, PROJECT_FOLDER, target_folder } from './const';
-import { exists } from './ls/asyncUtil';
+import { bin, binJs, layaAssets, project_folder, target_folder } from './const';
+import { exists, lstat } from './ls/asyncUtil';
 import { execArr } from './ls/exec';
 import { cp } from './ls/main';
 
 /** 图片 bin */
 export async function releaseAssets(commit?: string) {
     const files = await findChangeFiles(commit);
+    if (!files) {
+        console.log(`cant find change files!`);
+        return;
+    }
+
     for (const file of files) {
-        const ori_file = path.resolve(PROJECT_FOLDER, file);
+        const ori_file = path.resolve(project_folder, file);
         const dist_file = path.resolve(target_folder, file).replace('bin\\', '');
         cp(ori_file, dist_file).then(() => {
             console.log(`complete copy ${ori_file} => ${dist_file}`);
@@ -17,7 +22,7 @@ export async function releaseAssets(commit?: string) {
 }
 export async function findChangeFiles(commit?: string) {
     commit = commit || '';
-    const files_str = (await execArr(`git diff --name-only ${commit}`, { path: PROJECT_FOLDER })) as string;
+    const files_str = (await execArr(`git diff --name-only ${commit}`, { path: project_folder })) as string;
     if (!files_str) {
         return;
     }
@@ -26,16 +31,28 @@ export async function findChangeFiles(commit?: string) {
     files.pop();
     let result = [];
     for (const file of files) {
-        const target = await findTargetFile(file);
-        // console.log(file, target);
-        result = result.concat(target);
+        try {
+            const target = await findTargetFile(file);
+            if (Array.isArray(target)) {
+                result = result.concat(target);
+            } else {
+                result.push(target);
+            }
+        } catch (err) {
+            console.error(err);
+        }
     }
     result = result.filter((item, index) => {
         return result.indexOf(item) === index;
     });
     return result;
 }
-export async function findTargetFile(ori_file: string) {
+async function findTargetFile(ori_file: string) {
+    // 目标文件名 == 源文件名 ||  合并
+    const bin_file = await findBinFile(ori_file);
+    if (bin_file) {
+        return bin_file;
+    }
     // js + ui
     if (isScriptFile(ori_file) || isUIFile(ori_file)) {
         return [binJs];
@@ -43,29 +60,25 @@ export async function findTargetFile(ori_file: string) {
     if (isScriptFile(ori_file) || isUIFile(ori_file)) {
         return [binJs];
     }
-    // 目标文件名 == 源文件名 ||  合并
-    const bin_file = await findBinFile(ori_file);
-    if (bin_file) {
-        return bin_file;
-    }
     throw new Error(`cant find target file for ${ori_file}, maybe you should publish laya project`);
 }
 
-export function isScriptFile(ori_file: string) {
+function isScriptFile(ori_file: string) {
     if (ori_file.match(/(\.ts|\.js)$/)) {
         return true;
     }
     return false;
 }
-export function isUIFile(ori_file: string) {
+function isUIFile(ori_file: string) {
     if (ori_file.match(/^laya\/pages\//)) {
         return true;
     }
     return false;
 }
-export async function findBinFile(ori_file: string): Promise<string | string[]> {
+async function findBinFile(ori_file: string): Promise<string | string[]> {
     const bin_file = ori_file.replace(layaAssets, bin);
-    if (await exists(bin_file)) {
+
+    if (await exists(path.resolve(project_folder, bin_file))) {
         return bin_file;
     }
     const bin_folder = path.dirname(ori_file.replace(layaAssets, bin));
@@ -73,10 +86,10 @@ export async function findBinFile(ori_file: string): Promise<string | string[]> 
     const assets_json = `${bin_folder}.json`;
     const assets_png = `${bin_folder}.png`;
 
-    if (await exists(path.resolve(PROJECT_FOLDER, assets_json))) {
+    if (await exists(path.resolve(project_folder, assets_json))) {
         return [assets_json, assets_png];
     }
-    if (await exists(path.resolve(PROJECT_FOLDER, assets_atlas))) {
+    if (await exists(path.resolve(project_folder, assets_atlas))) {
         return [assets_atlas, assets_png];
     }
 }
